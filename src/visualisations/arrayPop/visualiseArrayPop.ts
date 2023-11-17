d733d4522f8d530bc7de0b3254b88da96ab91c09
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-11-17 14:32:17 +0000

    add non-animated log of array before the .pop() method is applied

--- src/visualisations/arrayPop/visualiseArrayPop.ts
+++ src/visualisations/arrayPop/visualiseArrayPop.ts
@@ -1,7 +1,9 @@
 import { arrayPopInfo } from './static/arrayPopInfo';
 import { horizontalRule } from '../../utils/horizontalRule';
+import { beforeArray } from './static/beforeArray';
 
 export const visualiseArrayPop = async () => {
   arrayPopInfo();
   horizontalRule();
+  beforeArray();
 };
