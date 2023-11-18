27c6d9fc2ab92e1d11dfac8f9722e6c66d801006
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-11-18 09:46:30 +0000

    add animated visualisation of the array when .pop() method is applied

--- src/visualisations/arrayPop/visualiseArrayPop.ts
+++ src/visualisations/arrayPop/visualiseArrayPop.ts
@@ -1,9 +1,11 @@
 import { arrayPopInfo } from './static/arrayPopInfo';
 import { horizontalRule } from '../../utils/horizontalRule';
 import { beforeArray } from './static/beforeArray';
+import { animatePop } from './animation/animatePop';
 
 export const visualiseArrayPop = async () => {
   arrayPopInfo();
   horizontalRule();
   beforeArray();
+  animatePop();
 };
