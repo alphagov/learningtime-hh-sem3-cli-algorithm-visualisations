120baf053b6af48cee6e7e3109a79628d49c88e5
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-12-01 11:13:52 +0000

    display pop metadata using metadata util

--- src/visualisations/arrayPop/visualiseArrayPop.ts
+++ src/visualisations/arrayPop/visualiseArrayPop.ts
@@ -1,10 +1,11 @@
-import { arrayPopInfo } from './static/arrayPopInfo';
 import { horizontalRule } from '../../utils/horizontalRule';
 import { beforeArray } from './static/beforeArray';
 import { animatePop } from './animation/animatePop';
+import { displayMetadata } from '../../utils/staticText/displayMetadata';
+import { METADATA } from '../../config';
 
 export const visualiseArrayPop = async () => {
-  arrayPopInfo();
+  displayMetadata(METADATA.pop.method, METADATA.pop.description);
   horizontalRule();
   beforeArray();
   animatePop();
