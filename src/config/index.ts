ad1c2aa9678b7f13a81ce8349fca3457fd7e5304
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-12-01 11:51:34 +0000

    update colours in config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,11 @@
 import chalk from 'chalk';
 
 export const SQUARE_BRACKET = chalk.hex('#da70d6');
-export const ARRAY_ELEMENT = chalk.hex('#b5cea8');
+export const ARRAY_ELEMENT_DEFAULT = chalk.hex('#b5cea8');
+export const GREEN = chalk.hex('#00FF00');
+export const RED = chalk.hex('#FF0000');
+
+export const EXAMPLE_ARRAY = [1, 2, 3, 4, 5];
 
 export const CHOICES = {
   pop: '.pop()',
