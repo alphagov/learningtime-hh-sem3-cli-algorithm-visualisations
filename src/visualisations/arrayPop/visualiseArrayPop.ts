e4dd57f4f0d5851a820a8976bed974216cb988a2
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-12-01 11:57:15 +0000

    Incorporate displayStaticArray and colouriseElements utils into pop animation

--- src/visualisations/arrayPop/visualiseArrayPop.ts
+++ src/visualisations/arrayPop/visualiseArrayPop.ts
@@ -1,12 +1,12 @@
 import { horizontalRule } from '../../utils/horizontalRule';
-import { beforeArray } from './static/beforeArray';
 import { animatePop } from './animation/animatePop';
 import { displayMetadata } from '../../utils/staticText/displayMetadata';
-import { METADATA } from '../../config';
+import { METADATA, EXAMPLE_ARRAY } from '../../config';
+import { displayStaticArray } from '../../utils/staticText/displayStaticArray';
 
 export const visualiseArrayPop = async () => {
   displayMetadata(METADATA.pop.method, METADATA.pop.description);
   horizontalRule();
-  beforeArray();
-  animatePop();
+  displayStaticArray(EXAMPLE_ARRAY);
+  animatePop(EXAMPLE_ARRAY);
 };
