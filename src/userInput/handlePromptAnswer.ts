39673fdebe35dfc245d7bdfefe634d30fc8cefa1
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-12-08 11:00:15 +0000

    remove not implemented bubblesort choice

--- src/userInput/handlePromptAnswer.ts
+++ src/userInput/handlePromptAnswer.ts
@@ -1,11 +1,8 @@
 import { CHOICES } from '../config/index';
 import { visualiseArrayPop } from '../visualisations/arrayPop/visualiseArrayPop';
-import { visualiseBubblesort } from '../visualisations/bubblesort/visualiseBubblesort';
 
 export const handlePromptAnswer = (choice: string) => {
   if (choice === CHOICES.pop) {
     visualiseArrayPop();
-  } else if (choice === CHOICES.bubblesort) {
-    visualiseBubblesort();
   }
 };
