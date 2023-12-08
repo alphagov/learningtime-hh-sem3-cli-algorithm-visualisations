13e0aa06c6b155c0d9af0dc0815efb34fdc0dc93
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-12-08 14:16:16 +0000

    update displayStaticArray to be able to change the colour of the title

--- src/utils/staticText/displayStaticArray.ts
+++ src/utils/staticText/displayStaticArray.ts
@@ -2,12 +2,17 @@ import chalk from 'chalk';
 import { ARRAY_ELEMENT_DEFAULT, SQUARE_BRACKET } from '../../config/index';
 import { colouriseElements } from '../coulouriseElements';
 
-export const displayStaticArray = (arr: number[]) => {
+export const displayStaticArray = (
+  title: string,
+  arr: number[],
+  colour?: chalk.Chalk,
+) => {
+  const colouredTitle = colour ? colour(title) : chalk.dim(title);
   const colourisedElements = colouriseElements(arr, ARRAY_ELEMENT_DEFAULT);
 
   console.log(
-    `${chalk.dim('Before:')}  ${SQUARE_BRACKET(
+    `${colouredTitle}  ${SQUARE_BRACKET(
       '[',
-    )}${colourisedElements}${SQUARE_BRACKET(']')}`,
+    )}${colourisedElements}${SQUARE_BRACKET(']')}\n`,
   );
 };
