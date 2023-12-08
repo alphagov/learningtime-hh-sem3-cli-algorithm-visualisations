09e0b1a7341f1cee719e6487b71d91d299f31b6e
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-12-08 14:18:19 +0000

    add bubblesort option to prompt answer handler which displays static information

---
+++ src/visualisations/bubblesort/visualiseBubblesort.ts
@@ -0,0 +1,17 @@
+import { horizontalRule } from '../../utils/horizontalRule';
+import { displayMetadata } from '../../utils/staticText/displayMetadata';
+import {
+  AFTER_TITLE,
+  BEFORE_TITLE,
+  DESCENDING_ARRAY,
+  GREEN,
+  METADATA,
+} from '../../config';
+import { displayStaticArray } from '../../utils/staticText/displayStaticArray';
+
+export const visualiseBubblesort = () => {
+  displayMetadata(METADATA.bubblesort.method, METADATA.bubblesort.description);
+  horizontalRule();
+  displayStaticArray(BEFORE_TITLE, DESCENDING_ARRAY);
+  displayStaticArray(AFTER_TITLE, DESCENDING_ARRAY, GREEN);
+};
